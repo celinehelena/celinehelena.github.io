fb197dbe96251ae62e8c378a1b0b1ef08df4a8e9
celinehelena <81686800+celinehelena@users.noreply.github.com>
2021-04-28 16:01:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ class Login extends React.Component {
 		super(props);
 		this.state = {
 			username: '',
-			password: ''
+			password: '',
+			token: '',
+      erro: false
 		};
 		//redefinindo um clique para quando o botao Login seja acionado
 		this.avaliar = this.avaliar.bind(this);
@@ -22,24 +24,37 @@ class Login extends React.Component {
 
 	handleLogin(event) {
 		event.preventDefault();
-		this.avaliar();
+
+		if(this.state.username || this.state.password){
+			this.avaliar();
+		}else{
+      this.setState({erro: true});
+			alert("Preencha os todos campos vazios");
+		}	
 	}
 
 	avaliar(){
+		//console.log(this.state);
 		fetch('http://colabeduc.org/api/login', {
 			method:'POST',
 			headers: {'Content-Type': 'application/json'},
 			body:JSON.stringify(this.state)
-		}).then(res => res.json())
-			.then(data => console.log(data))
-		.catch(ex => console.error('Problemas ao logar', ex));
+		}).then(r => r.json())
+			.then(json => {
+			  console.log(json);
+				this.setState({token: json.access_token});
+				alert("Login realizado com sucesso!");
+		}).catch(ex => console.error('Problemas ao logar', ex));
     	
 	}
 
 	render() {
 		return(
-			React.createElement("form", {method: "POST", onSubmit: this.handleLogin},
+			React.createElement("form", {method: "POST", onSubmit: this.handleLogin},           
 				React.createElement("h2", null, "Login"),
+                          
+         this.state.erro == true ?  React.createElement(SenhaErro, null) : null,    
+                          
 				React.createElement("div", { className: "input-container",placeholder: "senha" },  
 					React.createElement("label", null, "Login:  "),
 					React.createElement("input", {  className:"input",name: "username", type: "text", onChange: this.updateState}),
@@ -54,7 +69,7 @@ class Login extends React.Component {
 
 				React.createElement("div",{ className: "form-login" },
       				React.createElement("input", { type: "submit", className: "btn", value: "Entrar"}), "   ",
-     				React.createElement("p",null, "Não tem conta? ", React.createElement("a", {href: "cadastro.html"}, " Cadastro"))
+     				React.createElement("p",null, "Não tem conta? ", React.createElement("a", {href: "cadastro.html"}, " Cadastre-se"))
      			)
 
 
@@ -64,6 +79,18 @@ class Login extends React.Component {
 	}
 }
 
+class SenhaErro extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      React.createElement("div", {className: "senha"},
+        React.createElement("span", null, "Usuário e senha inválidos")
+      )
+    );
+  }
+}
 
 ReactDOM.render(
   React.createElement(Login, null),
